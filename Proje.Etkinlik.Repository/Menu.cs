53e03bfcf60c017a47ec109aa1bc2b751bac9e0f
yigitbuyukkasap <yigitbuyukkasap@gmail.com>
2022-04-21 15:50:07 +0300

    adding menu to repo

---
+++ Proje.Etkinlik.Repository/Menu.cs
@@ -0,0 +1,31 @@
+﻿using Proje.Etkinlik.Data;
+using System.Linq;
+
+namespace Proje.Etkinlik.Repository
+{
+    public class Menu
+    {
+        Proje_EtkinlikEntities entities = new Proje_EtkinlikEntities();
+
+        public int Ekle(Data.Menu obj)
+        {
+            entities.Menu.Add(obj);
+            int returnValue = entities.SaveChanges();
+            return returnValue;
+        }
+
+        public void Duzenle(Data.Menu obj)
+        {
+            var ent = entities.Menu.Where(p => p.id ==  obj.id).FirstOrDefault();
+            ent = obj;
+            var sonuc = entities.SaveChanges();
+
+        }
+        public void Sil(int id)
+        {
+            var silincekData = entities.Menu.Where(p => p.id == id).FirstOrDefault();
+            entities.Menu.Remove(silincekData);
+            var sonuc = entities.SaveChanges();
+        }
+    }
+}
